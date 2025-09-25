9ae91831455f62e0baf74487823456f0cd67e340
Bruno Ravanhani <bruno.ravanhanif@gmail.com>
2025-09-25 14:37:06 -0400

    add terraform database

---
+++ terraform/database/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  tags = {
+    Terraform = "true"
+    Context   = "ravanhani-site"
+    App       = "children-movies"
+  }
+}
+
+
+resource "aws_dynamodb_table" "table_creation" {
+  name           = var.table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
+}
+
+resource "null_resource" "populate_data" {
+  depends_on = [aws_dynamodb_table.table_creation]
+
+  provisioner "local-exec" {
+    command = "dotnet run --project ../../src/MoviesDataLoad/MoviesDataLoad.csproj -- ${var.table_name}"
+  }
+}
---
+++ terraform/database/variables.tf
@@ -0,0 +1,5 @@
+variable "table_name" {
+  description = "DynamoDB Table Name"
+  type        = string
+  default     = "children-movies-api-database"
+}
